83118efdac3b263da79071afc00ab752db73c75d
phatvphat <phatvphat@gmail.com>
2021-09-20 22:49:16 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express'), app = express();
+const bodyParser = require('body-parser');
+var cors = require('cors');
 const port = process.env.PORT || 3002;
 var path = require('path');
 var useragent = require('express-useragent');
-var fs = require('fs');
-const FFmpeg = require('fluent-ffmpeg');
-const { PassThrough } = require('stream');
+const ytdl = require('ytdl-core')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+const { PassThrough } = require('stream')
+const fs = require('fs')
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(useragent.express());
 
@@ -36,10 +44,35 @@ app.get('/user_agent', function (req, res) {
 app.get('/audio/:v', function (req, res) {
 	var vid = req.params.v;
 	if (vid != '') {
-		var youtubeStream = require('youtube-audio-stream');
+		// var youtubeStream = require('youtube-audio-stream');
 		var requestUrl = 'http://youtube.com/watch?v=' + vid;
 		try {
-			youtubeStream(requestUrl).pipe(res);
+			// youtubeStream(requestUrl).pipe(res);
+			var opt = {
+				...opt,
+				videoFormat: 'mp4',
+				quality: 'lowest',
+				audioFormat: 'mp3',
+				filter(format) { return format.container === opt.videoFormat && format.audioBitrate }
+			}
+			const video = ytdl(requestUrl, opt)
+			const { file, audioFormat } = opt
+			const stream = file ? fs.createWriteStream(file) : new PassThrough()
+			const ffmpeg_ = new ffmpeg(video)
+
+			process.nextTick(() => {
+				const output = ffmpeg_.format(audioFormat).pipe(stream).pipe(res);
+				ffmpeg_.on('error', error => stream.emit('error', error));
+				output.on('error', error => {
+					video.end()
+					stream.emit('error', error)
+				});
+				output.on('finish', (src) => {
+					console.log('Something is piping into the writer.');
+				});
+			});
+			// stream.pipe(res);
+			// stream.unpipe(res);
 		} catch (exception) {
 			res.status(500).send(exception);
 		}
@@ -67,16 +100,16 @@ app.get('/video', function (req, res) {
 app.get('/:v', async function (req, res) {
 	var vid = req.params.v;
 	if (vid != '') {
-		var ytdl = require('ytdl-core');
-		var requestUrl = 'http://youtube.com/watch?v=' + vid;
-		var info = await ytdl.getBasicInfo(requestUrl);
-		info = info.videoDetails;
-		// console.log(info);
-		res.send({
-			title: info.title, author: info.author, duration: sec2time(info.lengthSeconds),
-			thumbnail: info.thumbnail.thumbnails,
-			stream: { audio: req.protocol + '://' + req.get('host') + '/audio/' + vid, video: null }
-		});
+		try {
+			var info = await ytdl.getBasicInfo(vid);
+			info = info.videoDetails;
+			// console.log(info);
+			res.send({
+				title: info.title, author: info.author, duration: sec2time(info.lengthSeconds),
+				thumbnail: info.thumbnail.thumbnails,
+				stream: { audio: req.protocol + '://' + req.get('host') + '/audio/' + vid, video: null }
+			});
+		} catch (e) { res.send({}); }
 	} else { res.send({}); }
 });
 app.all('/', async function (req, res) { res.send({}); });
